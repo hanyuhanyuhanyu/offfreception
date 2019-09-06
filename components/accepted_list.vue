654f80c94cbfabe13fc46412c14f99547b9c3c5c
Amy with dogs <concon.wolf@gmail.com>
2019-09-06 22:50:47 +0900

    aoeus

---
+++ components/accepted_list.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="lst">
+    <div style="height: 80px; top: 0; left: 0; font-size: 2rem; vertical-align: middle; display: flex; align-items: center;">
+      受付履歴
+    </div>
+    <li style="position: relative" v-for="usr in list">
+      {{usr.handleName}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default{
+  props: {
+    list: {
+      type: Array,
+      default: [],
+    }
+  }
+}
+</script>
+
+<style>
+.lst {
+  width: 320px;
+  text-align: left;
+}
+li {
+  list-style-type: none;
+  left: 0;
+}
+</style>
